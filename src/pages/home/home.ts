326d7bb8aaa1ea216bc51117bdac6c25339b557f
elvynia <elvynia@gmail.com>
2018-04-06 12:00:43 +0200

    Implémentation de la page Home.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { StockManagementPage } from '../stock-management/stock-management';
+import { ProductManagementPage } from '../product-management/product-management';
+
 @Component({
 	selector: 'page-home',
 	templateUrl: 'home.html'
 })
 export class HomePage {
+	marketValid: boolean
 
 	constructor(public navCtrl: NavController) {
-
+		this.marketValid = false;
 	}
 
+	canValidate(event) {
+		this.marketValid = event.currentTarget.value.length === 5;
+	}
+
+	goToStock() {
+		this.navCtrl.push(StockManagementPage);
+	}
 }
