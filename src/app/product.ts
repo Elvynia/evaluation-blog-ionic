13e17e3db428290baf239556a3e738daf0e620e4
elvynia <elvynia@gmail.com>
2018-04-06 12:01:20 +0200

    Implémentation de la page de gestion du stock.

--- src/app/product.ts
+++ src/app/product.ts
@@ -4,6 +4,13 @@ export class Product {
 	stock: number;
 	threshold: number;
 
+	constructor(id?: string, label?: string, stock: number = 0, threshold: number = 0) {
+		this.id = id;
+		this.label = label;
+		this.stock = stock;
+		this.threshold = threshold;
+	}
+
 	public get stockSafe(): boolean {
 		return this.stock > this.threshold;
 	}
--- src/pages/stock-management/stock-management.ts
+++ src/pages/stock-management/stock-management.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { Product } from '../../app/product';
+import { ProductProvider } from '../../providers/product/product';
+
 @Component({
 	selector: 'page-stock-management',
 	templateUrl: 'stock-management.html',
 })
 export class StockManagementPage {
+	products: Array<Product>;
 
-	constructor(public navCtrl: NavController, public navParams: NavParams) { }
+	constructor(public navCtrl: NavController, public navParams: NavParams,
+		private productService: ProductProvider) {
+		this.products = new Array();
+	}
 
+	ngOnInit() {
+		this.refresh();
+		// Utilisé pour debug/test.
+		this.products.push(new Product('00001', 'Chips barbecue', 150, 40));
+	}
+
+	updateStock(product: Product, quantity: number) {
+		product.stock -= quantity;
+		this.productService.update(product);
+		this.refresh();
+	}
+
+	private refresh() {
+		this.products = this.productService.getAll();
+	}
 }
