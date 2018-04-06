95fed8e0e6e86dc6d9ff0a4229256600c6b9b6fc
elvynia <elvynia@gmail.com>
2018-04-06 10:48:18 +0200

    Implémentation de service/provider pour gérer et maintenir une collection de produits.

--- src/providers/product/product.ts
+++ src/providers/product/product.ts
@@ -1,9 +1,52 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+import { Product } from '../../app/product';
 
 @Injectable()
 export class ProductProvider {
+	private products: Array<Product>;
 
-	constructor(public http: HttpClient) { }
+	constructor() {
+		this.products = new Array();
+	}
 
+	public getAll(): Array<Product> {
+		return this.products;
+	}
+
+	public create(product: Product) {
+		if (this.exists(product.id)) {
+			throw new Error('Cannot create a new product with existing id ' + product.id);
+		} else {
+			this.products.push(product);
+		}
+	}
+
+	public read(id: string): Product {
+		return this.products.find((p: Product) => p.id === id);
+	}
+
+	public update(product: Product) {
+		if (this.exists) {
+			this.products.splice(this.getIndex(product.id), 1, product);
+		} else {
+			throw new Error('Cannot update product with unknown id ' + product.id);
+		}
+	}
+
+	public delete(id: string) {
+		if (this.exists) {
+			this.products.splice(this.getIndex(id), 1);
+		} else {
+			throw new Error('Cannot delete product with unknown id ' + id);
+		}
+	}
+
+	private exists(id: string): boolean {
+		return this.getIndex(id) >= 0;
+	}
+
+	private getIndex(id: string): number {
+		return this.products.findIndex((p: Product) => p.id === id);
+	}
 }
